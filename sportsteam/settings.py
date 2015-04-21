f015f53065ec5e8e2be6fc44b1f3b7c33e536acf
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2015-04-21 18:38:11 +0300

    Use timezone

--- sportsteam/settings.py
+++ sportsteam/settings.py
@@ -35,6 +35,7 @@ DATABASES = {
 # If running in a Windows environment this must be set to the same as your
 # system time zone.
 TIME_ZONE = 'GMT'
+USE_TZ = True
 
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
